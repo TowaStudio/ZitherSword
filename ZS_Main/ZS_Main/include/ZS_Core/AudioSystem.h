a127305b8c282b3aa990e8991a23b8ff33de0263
zty <ec,mikezhao@gmail.com>
2017-02-15 23:17:07 +0800

    basic AS framework

--- ZS_Main/ZS_Main/include/ZS_Core/AudioSystem.h
+++ ZS_Main/ZS_Main/include/ZS_Core/AudioSystem.h
@@ -8,8 +8,63 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 
 namespace ZS {
+	enum NoteName {
+		REST = 0,
+		DO = 1,
+		RE = 2,
+		MI = 3,
+		FA = 4,
+		SO = 5,
+		LA = 6,
+		SI = 7
+	};
+	struct Note {
+		NoteName name = REST;
+		float value = 1; // 1 = 1 meter
+		float startMoment = 0; // 1 = 1 meter
+		int barNum = 1;
+	};
+
+
 	class AudioSystem : public Timer{
+	public:
+		static AudioSystem* GetInstance() {
+			return instance;
+		}
+
+		void musicSetup(int timePerBeat, int beatsPerBar);
+		void startMusic(); 
+		void stopMusic();
+		void input(NoteName inputKey);
+
+		// get
+		float getBpm() {
+			return bpm;
+		}
+		float getTolerance() {
+			return tolerance;
+		}
+
+
 	private:
+		int bpm; // beats per minute
+		int bpb; // beats per bar
+
+		float tolerance; // 0: no tolerance - 0.5: full tolerance 
+		int tpb; // ticks per beat
+		int interval;
+		int thresTime;
+
+
+		//int64 startTime;
+		int currentBarNum; // the current number of bar
+		int currentTickNum; // the current number of tick within a bar
+		int64 currentTickTime; 
+		//int64 nextTickTime;
+
+		void inputJudge(int64 currentTime, NoteName noteName);
+		void recordNote(int tickNum, NoteName noteName);
+
 		static AudioSystem* instance;
 		AudioSystem();
 		AudioSystem(AudioSystem const&) {};
@@ -17,30 +72,6 @@ namespace ZS {
 
 		void timerCallback() override;
 
-	public:
-		static AudioSystem* GetInstance() {
-			return instance;
-		}
-
-		bool timerStarted; // read only
-
-		enum UserInput
-		{
-			Idle = 0,
-			Do = 1, 
-			Re = 2, 
-			Mi = 3,
-			So = 5, 
-			La = 6
-		};
-
-		//void timerStart() {
-		//	startTimer(1000);
-		//};
-
-		void input(UserInput inputKey);
-
-		int time;
 	};
 }
 
--- ZS_Main/ZS_Main/src/ZS_Core/AudioSystem.cpp
+++ ZS_Main/ZS_Main/src/ZS_Core/AudioSystem.cpp
@@ -11,11 +11,81 @@ namespace ZS {
 	*/
 	AudioSystem* AudioSystem::instance = new AudioSystem();
 
+	void AudioSystem::musicSetup(int _bpm = 92, int _bpb = 4) {
+		// setting properties
+		bpm = _bpm;
+		bpb = _bpb;
+
+		// hardcoding properties
+		tolerance = 0.25;
+		tpb = 4;
+
+		// calculating properties
+		interval = (60 * 1000) / (tpb * bpm);
+		thresTime = tolerance * interval;
+	}
+
+	void AudioSystem::startMusic() {
+		// init
+		currentBarNum = 0;
+		currentTickNum = -1;
+		currentTickTime = -1;
+		//nextTickTime = -1;
+
+		// start timer
+		//startTime = Time::currentTimeMillis();
+		startTimer(interval);
+	}
+
+	void AudioSystem::stopMusic() {
+		stopTimer();
+	}
+
+	void AudioSystem::input(NoteName inputNote) {
+
+		// judge input
+		if (isTimerRunning()) {
+			int64 currentTime = Time::currentTimeMillis();
+			inputJudge(currentTime, inputNote);
+		}
+
+		// play sound : TODO
+
+	}
+
+	void AudioSystem::inputJudge(int64 currentTime, NoteName noteName) {
+		int timeDiff = currentTime - currentTickTime;
+		if (timeDiff < thresTime) { // current tick
+			recordNote(currentTickNum, noteName);
+		}
+		else if (timeDiff > interval - thresTime) { // next tick
+			recordNote(currentTickNum + 1, noteName);
+		}
+		else { // hit nothing
+
+		}
+	}
+
+	void AudioSystem::recordNote(int tickNum, NoteName noteName) {
+		// TODO
+	}
+
 	AudioSystem::AudioSystem() {
-		time = 0;
+		musicSetup();
 	}
 	
 	void AudioSystem::timerCallback() {
-		time++;
+		if (currentTickNum + 1 - tpb * bpb >= 0) {
+			currentBarNum += 1; 
+			currentTickNum = 0;
+			// TODO: sequence identify
+		}
+		else {
+			currentTickNum++;
+		}
+		currentTickTime = Time::currentTimeMillis();
+		//nextTickTime = currentTickTime + interval;
+
+		// TODO: play solid music
 	}
 }
